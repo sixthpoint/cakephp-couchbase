6ab78a5acf5d95531774994ddd9b23b2d3c5d279
sixthpoint <sixthpoint@192.168.0.104>
2014-05-16 23:43:29 -0500

    corrected readme

--- Config/routes.php
+++ Config/routes.php
@@ -31,66 +31,6 @@
  * ...and connect the rest of 'Pages' controller's urls.
  */
 // Include custom subdomain routes class
-App::uses('SubdomainRoute', 'Routes');
-
-Router::mapResources('api');
-//Router::mapResources('churches');
-Router::mapResources('artists');
-Router::mapResources('albums');
-Router::parseExtensions('json');
-
-// Use subdomain routing to determine what kind of subdomain route we have (church or station). Only executes if we have a subdomain
-if (Configure::read('SubdomainHTTP.subdomain')) {
-
-    $subdomainRouting = new SubdomainRoute();
-}
-
-// Set basic pages for IRDB.FM
-Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
-Router::connect('/streaming', array('controller' => 'pages', 'action' => 'streaming'));
-Router::connect('/webdesign', array('controller' => 'pages', 'action' => 'webdesign'));
-Router::connect('/networking', array('controller' => 'pages', 'action' => 'networking'));
-Router::connect('/techsupport', array('controller' => 'pages', 'action' => 'techsupport'));
-Router::connect('/freequote', array('controller' => 'pages', 'action' => 'freequote'));
-Router::connect('/help', array('controller' => 'pages', 'action' => 'help'));
-Router::connect('/services', array('controller' => 'pages', 'action' => 'services'));
-Router::connect('/contactus', array('controller' => 'pages', 'action' => 'contactus'));
-Router::connect('/aboutus', array('controller' => 'pages', 'action' => 'aboutus'));
-Router::connect('/computerwarranty', array('controller' => 'pages', 'action' => 'computerwarranty'));
-Router::connect('/churchoffer', array('controller' => 'pages', 'action' => 'churchoffer'));
-
-
-if (Configure::read('Subdomain.sub_bus_id') != NULL) {
-
-    // Setup routes for Church Pages
-    Router::connect('/', array('controller' => 'businesses', 'action' => 'web_player'));
-    
-    Router::connect('/data_widget/:size', array('controller' => 'businesses', 'action' => 'data_widget'), array('pass' => array('size'), 'size' => '[0-9]+'));
-    Router::connect('/audio_widget/*', array('controller' => 'businesses', 'action' => 'audio_widget'));
-
-    Router::connect('/recordings/*', array('controller' => 'businesses', 'action' => 'recordings'));
-    Router::connect('/stream/*', array('controller' => 'streams', 'action' => 'status'));
-
-    // both need moved to a better location
-    Router::connect('/schedule/*', array('controller' => 'businesses', 'action' => 'schedule'));
-} else {
-    // Default route for IRDB.FM with no subdomain set
-    Router::connect('/', array('controller' => 'pages', 'action' => 'index'));
-}
-
-
-// Music Recognition System Routing
-Router::connect('/albums/:artist/:album', array('controller' => 'albums'), array('pass' => array('artist', 'album')));
-Router::connect('/artists/:artist', array('controller' => 'artists'), array('pass' => array('artist')));
-
-
-
-// Marked for deletion! 7/7/13
-//Router::connect('/stations/:action/', array('controller' => 'stations'));
-//Router::connect('/stations/artists/:action/:id/:artist', array('controller' => 'artists'), array('pass' => array('id', 'artist'), 'id' => '[0-9]+'));
-//Router::connect('/mobile/:action/:id-:slug', array('controller' => 'mobile'), array('pass' => array('id', 'slug'), 'id' => '[0-9]+'));
-
-
 
 
 
--- README.md
+++ README.md
@@ -1,43 +1,23 @@
-CakePHP
+CakePHP - Couchbase Datasource
 =======
 
 [![CakePHP](http://cakephp.org/img/cake-logo.png)](http://www.cakephp.org)
 
-CakePHP is a rapid development framework for PHP which uses commonly known design patterns like Active Record, Association Data Mapping, Front Controller and MVC.
-Our primary goal is to provide a structured framework that enables PHP users at all levels to rapidly develop robust web applications, without any loss to flexibility.
+The following is a simple datasource solution for couchbase server 2. This is intended to work with CakePHP 2.x
 
-Some Handy Links
+Background
 ----------------
 
-[CakePHP](http://www.cakephp.org) - The rapid development PHP framework
+Sometimes your app has the need for large data sets, and require the flexibility of NoSQL to accomplish this. There is no couchbase datasource available / and or they are overly complicated. This is the simple solution.
 
-[CookBook](http://book.cakephp.org) - THE CakePHP user documentation; start learning here!
-
-[API](http://api.cakephp.org) - A reference to CakePHP's classes
-
-[Plugins](http://plugins.cakephp.org/) - A repository of extensions to the framework
-
-[The Bakery](http://bakery.cakephp.org) - Tips, tutorials and articles
-
-[Community Center](http://community.cakephp.org) - A source for everything community related
-
-[Training](http://training.cakephp.org) - Join a live session and get skilled with the framework
-
-[CakeFest](http://cakefest.org) - Don't miss our annual CakePHP conference
-
-[Cake Software Foundation](http://cakefoundation.org) - Promoting development related to CakePHP
-
-Get Support!
+Requirements
 ------------
 
-[#cakephp](http://webchat.freenode.net/?channels=#cakephp) on irc.freenode.net - Come chat with us, we have cake
+* CakePHP 2.x
+* Couchbase Server 2.x
+* PHP5
 
-[Google Group](https://groups.google.com/group/cake-php) - Community mailing list and forum
+Installation
+------------
 
-[GitHub Issues](https://github.com/cakephp/cakephp/issues) - Got issues? Please tell us!
-
-[Roadmaps](https://github.com/cakephp/cakephp/wiki#roadmaps) - Want to contribute? Get involved!
-
-[![Bake Status](https://secure.travis-ci.org/cakephp/cakephp.png?branch=master)](http://travis-ci.org/cakephp/cakephp)
-
-![Cake Power](https://raw.github.com/cakephp/cakephp/master/lib/Cake/Console/Templates/skel/webroot/img/cake.power.gif)
+TODO: write
